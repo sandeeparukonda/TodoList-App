19a55e5b2d3c252daa33834a1139e91992d98b7b
sandeeparukonda <sandeeparukonda21@gmail.com>
2023-02-17 13:02:19 +0530

    TodoList-App

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React,{useState,useEffect}  from "react";
+
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodoList from "./components/TodoLists";
+
+const App = () =>{
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const[input,setInput] = useState("");
+  const[todos,setTodos] = useState(initialState);
+  const[editTodo,setEditTodo] = useState(null)
+
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos));
+  },[todos]);
+  return(
+    <center>
+    <div>
+      <Header/>
+    </div>
+    <div>
+      <Form
+      input={input}
+      setInput={setInput}
+      todos={todos}
+      setTodos={setTodos}
+      editTodo={editTodo}
+      setEditTodo={setEditTodo}
+      />
+    </div>
+    <div>
+      <TodoList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo}/> 
+    </div>
+    </center>
+  )
+}
+
+export default App
